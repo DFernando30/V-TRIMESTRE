27e5f871fe1bad67a77d1582a8335c4e6f7e1384
DFernando30 <dfbolanos16@misena.edu.co>
2023-09-06 18:25:28 -0500

    EJERCICIO MERN PARTE 1

---
+++ JAVIER/EJERCICIOS_JAVIER/MERN_STACK/src/routes/task.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const Task = require('../models/task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+router.post('/', async (req, res) => {
+    const {title, description} = req.body;
+    const task = new Task({title,description});
+    await task.save();
+    res.json({status: 'Tarea Guardada'});
+});
+
+module.exports = router;
